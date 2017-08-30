130eb48b8a1bc725a6ae6f039c34aac48ea64019
Hidehiko Kondo <dev@udonko.net>
2017-08-30 14:52:03 +0900

    コード取り込み

--- RanningAlarm/RanningAlarm/ViewController.h
+++ RanningAlarm/RanningAlarm/ViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<AVAudioPlayerDelegate, UIPickerViewDelegate, UIPickerViewDataSource>
 
+@property(nonatomic) AVAudioPlayer *audioPlayer;
 
 @end
 
