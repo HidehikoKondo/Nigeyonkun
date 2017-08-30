ab164b307bff980b5d793e8c4b7285c8db967b2c
Hidehiko Kondo <dev@udonko.net>
2017-08-30 20:07:30 +0900

    プロジェクト再作成

---
+++ RunningAlarm/RunningAlarm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunningAlarm
+//
+//  Created by Hidehiko Kondo on 2017/08/30.
+//  Copyright © 2017年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunningAlarm/RunningAlarm/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RunningAlarm
+//
+//  Created by Hidehiko Kondo on 2017/08/30.
+//  Copyright © 2017年 UDONKONET. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
